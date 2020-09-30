042cc4aec026da724960d966f52c65145156aebe
aneesh babu <bob@example.com>
2020-09-30 22:51:00 +0530

    adding files

---
+++ Practice1/src/main/java/com/SpringBootPractice/Practice1/Controller/PresentationLayerController.java
@@ -0,0 +1,23 @@
+package com.SpringBootPractice.Practice1.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PresentationLayerController {
+	
+	
+	@GetMapping(value="/helloworld")
+	private static String  getHelloWorld() {
+		return "HELLO WORLD";
+	}
+	
+	@GetMapping(value="/pathvariable/{value}")
+	private static String getPathVariable(@PathVariable(value="value") String values)
+	{
+		
+		return values;
+	}
+	
+}
